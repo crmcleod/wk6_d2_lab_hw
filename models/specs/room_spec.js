3e4d12f5f3bcb488dd88c7e96c80cee5d586ee20
Craig McLeod <mcleod.craig@outlook.com>
2020-06-16 16:23:00 +0100

    Initialish commit

---
+++ models/specs/room_spec.js
@@ -0,0 +1,15 @@
+const assert = require('assert')
+const Room = require('../../room.js')
+
+describe('Room', function(){
+    let room
+    beforeEach(function(){
+        room = new Room(1000)
+    })
+    describe ('Properties', function(){
+        it('should have an area', function(){
+            const actual = room.area
+            assert.strictEqual(actual, 1000)
+        })
+    })
+})
